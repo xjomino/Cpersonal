1280dfb048973a3cf11ef555e61de609f11aaaf4
xjomino <111028708+xjomino@users.noreply.github.com>
2022-09-03 12:23:36 +0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,6 @@
-#include <stdio.h>  /*This ia processor directive that asks the C processor to look for and place the library in that 				C program. stdio.h file stands for input-output header file that contains numerous prototypes and macros to perform (I/O) for C programs */
+#include <stdio.h>  /*This ia processor directive that asks the C processor to look for and place 
+			the library in that C program. stdio.h file stands for input-output header 
+			file that contains numerous prototypes and macros to perform (I/O) for C programs */
 int main ()
 
 {
