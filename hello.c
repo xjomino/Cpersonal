68d3002c823b3154c96a9fb6024e781e57eec129
xjomino <jammwa@gmail.com>
2022-09-03 09:21:45 +0000

    my first C

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h>  /*This ia processor directive that asks the C processor to look for and place the library in that 				C program. stdio.h file stands for input-output header file that contains numerous prototypes and macros to perform (I/O) for C programs */
 int main ()
 
 {
 	printf ("Hello, world. \n");
+	return 0;
 
 }
